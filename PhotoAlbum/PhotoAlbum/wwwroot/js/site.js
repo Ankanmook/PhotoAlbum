6602389e1fa8d196babb861c3ffc3444e37aa857
CLARKSONS\amj <amj@clarksons.com>
2018-09-20 17:18:15 +0100

    client side html and code, horrendous, too much time to process data this needs pagination

--- PhotoAlbum/PhotoAlbum/wwwroot/js/site.js
+++ PhotoAlbum/PhotoAlbum/wwwroot/js/site.js
@@ -2,3 +2,25 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    $('#photoAlbumTable').DataTable({
+        "ajax":
+        {
+            "url": "/PhotoAlbum/Get",
+            "type": "GET",
+            "datatype": "json"
+        },
+        columns: [
+            { AlbumId: "AlbumId" },
+            { PhotoId: "PhotoId" },
+            { PhotoTitle: "Photo Title" },
+            { AlbumName: "Album Name" },
+            { ThumbnailUrl: "Thumbnail Image", render: getImg }
+        ]
+    });
+
+    function getImg(data, type, full, meta) {
+        return '<a target="_blank" href="'+full[5]+'"> <img src="'+data+'"></a>';
+    }
+
+});
