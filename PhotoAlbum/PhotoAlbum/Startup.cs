944bc97c8afcf552c3a0ff880890c75da2826470
CLARKSONS\amj <amj@clarksons.com>
2018-09-20 17:35:37 +0100

    changing start up to show this page. This concludes phase 1 dev

--- PhotoAlbum/PhotoAlbum/Startup.cs
+++ PhotoAlbum/PhotoAlbum/Startup.cs
@@ -67,7 +67,7 @@ namespace PhotoAlbum
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=PhotoAlbum}/{action=Index}/{id?}");
             });
         }
     }
