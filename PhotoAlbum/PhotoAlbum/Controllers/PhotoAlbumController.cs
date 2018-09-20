c61b62e67bee9d719be1e694d94070180cad8108
DESKTOP-L2JTK77\tupai <tupaimook>
2018-09-20 08:29:33 +0100

    added photo album controller

---
+++ PhotoAlbum/PhotoAlbum/Controllers/PhotoAlbumController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoAlbum.Controllers
+{
+    public class PhotoAlbumController : Controller
+    {
+        
+    }
+}
